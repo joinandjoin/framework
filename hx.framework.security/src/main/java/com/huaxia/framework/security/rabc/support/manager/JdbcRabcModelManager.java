77cd1161206b0db3b3530ec42d576198b3f4c450
shilei <shilei@shilei-pc.hxxc.com>
2017-05-10 21:38:24 +0800

    SECOND

---
+++ hx.framework.security/src/main/java/com/huaxia/framework/security/rabc/support/manager/JdbcRabcModelManager.java
@@ -0,0 +1,25 @@
+package com.huaxia.framework.security.rabc.support.manager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.huaxia.framework.common.database.repository.ICommonDAO;
+import com.huaxia.framework.common.enums.privilege.RabcModelType;
+import com.huaxia.framework.security.rabc.manager.IRabcModelManager;
+
+public class JdbcRabcModelManager implements IRabcModelManager {
+	
+	@Autowired
+	private ICommonDAO commonDao;
+	
+	@Override
+	public void createOrUpdateModel(RabcModelType type, String modelCode, String modelName, long modelCate,
+			String modelValue) {
+		createOrUpdateParty(modelCode, modelName, modelCate, modelValue);
+		
+	}
+
+	private void createOrUpdateParty(String modelCode, String modelName, long modelCate,
+			String modelValue){
+		
+	}
+}
